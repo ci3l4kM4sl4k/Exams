80de7fb14c756641bcd3d686e1374acc7cb93abb
ci3l4kM4sl4k <dubieltomasz@protonmail.com>
2022-09-17 19:07:34 +0200

    Update permutation.cpp

--- permutation.cpp
+++ permutation.cpp
@@ -1,8 +1,40 @@
+//Algorithm that for a given sequence of n positive integers stored in an array will calculate 
+//the smallest amount of elements that must be replaced in it to obtain an n-permutation
 #include <iostream>
 using namespace std;
 
+int permutation(int n, int arr[])
+{
+    int q = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            if(arr[i] )
+        }
+    }
+    
+    return q;
+}
+
 int main()
 {
-  
-  return 0;
+    int arr[10];
+    int n = 0, q = 0;
+
+    cout << "What is the amount of numbers: ";
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << "Enter " << i+1 << ". number: ";
+        cin >> arr[i];
+    }
+
+    q = permutation(n, arr);
+
+    cout << "Smallest amount of elements that must be replaced in order to obtain n-permutation is: " << q;
+
+    return 0;
 }
