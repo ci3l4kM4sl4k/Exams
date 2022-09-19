28394d7ee5ab72c6e4200f5086816ab60ced0322
ci3l4kM4sl4k <dubieltomasz@protonmail.com>
2022-09-19 19:22:51 +0200

    Update 20_05_2022__4_2.cpp

--- 20_05_2022__4_2.cpp
+++ 20_05_2022__4_2.cpp
@@ -1,9 +1,32 @@
+//Finds the number from 'number.txt' file with the most prime factors
+//and the number with the most distinct prime factors
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main()
 {
+    string fileName = "przyklad.txt";
+    fstream file;
+    int n;
 
+    file.open(fileName, ios::in);
+        while(!file.eof())
+        {
+            file >> n;
+            cout << n << endl;
+
+            for(int d = 2; d < n; d++)
+            {
+                while(n % d == 0)
+                {
+                    n /= d;
+                    cout << d << "\t";
+                }
+            }
+            cout << n << endl << endl;
+        }
+    file.close();
 
     return 0;
 }
