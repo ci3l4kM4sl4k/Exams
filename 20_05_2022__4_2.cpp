857542f68dd80865fc785a7e5e33e9196118b0cb
ci3l4kM4sl4k <dubieltomasz@protonmail.com>
2022-09-19 19:36:40 +0200

    Update 20_05_2022__4_2.cpp

--- 20_05_2022__4_2.cpp
+++ 20_05_2022__4_2.cpp
@@ -8,25 +8,47 @@ int main()
 {
     string fileName = "przyklad.txt";
     fstream file;
-    int n;
+    int n, number, maxDivisors=0, number2, maxDiffDivisors=0;
 
     file.open(fileName, ios::in);
         while(!file.eof())
         {
+            int tmp,  divisors = 0,  diffDivisors = 0;
+
             file >> n;
             cout << n << endl;
 
-            for(int d = 2; d < n; d++)
+            tmp = n;
+
+            int d = 2;
+            while(d < n)
             {
                 while(n % d == 0)
-                {
+                {                    
+                    divisors++;
                     n /= d;
                     cout << d << "\t";
                 }
+
+                d++;
+
+                if(n % d == 0)
+                    diffDivisors++;
             }
             cout << n << endl << endl;
+
+            if(divisors > maxDivisors)
+                number = tmp;
+
+            if(diffDivisors > maxDiffDivisors)
+                number2 = tmp;
+                
+            
         }
     file.close();
+
+    cout << "\nNumber with highiest amount of prime divisors: " << number << " (" << maxDivisors << ")\n";
+    cout << "\nNumber with highiest amount of prime divisors: " << number2 << " (" << maxDiffDivisors << ")\n";
 
     return 0;
 }
