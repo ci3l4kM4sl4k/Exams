0d06efee006ebc218f0a6f467ab4242b226c041d
ci3l4kM4sl4k <dubieltomasz@protonmail.com>
2022-09-20 23:22:48 +0200

    Update 20_05_2022__4_3.cpp

--- 20_05_2022__4_3.cpp
+++ 20_05_2022__4_3.cpp
@@ -7,11 +7,11 @@ using namespace std;
 
 int main()
 {
-    string fileName = "przyklad.txt", fileName1 = "trujki.txt";
+    string fileName = "numbers.txt", fileName1 = "triples.txt"; //names of files
     fstream file, file1;
     int arr[200], amountThrees = 0, amountFives = 0;
 
-    file.open(fileName, ios::in);
+    file.open(fileName, ios::in); //imports all numbers from the first file
         for(int i = 0; i < 200; i++)
             file >> arr[i];
     file.close();
@@ -21,9 +21,10 @@ int main()
             for(int z = 0; z < 200; z++)
                 if(arr[z] % arr[j] == 0 && arr[j] % arr[i] == 0 && arr[i] != arr[j] && arr[j] != arr[z])
                 {
-                    file1.open(fileName1, ios::app);
+                    file1.open(fileName1, ios::app); //exports all triples of numbers to secound file
                         file1 << arr[i] << "\t" << arr[j] << "\t" << arr[z] << endl;
                     file1.close();
+
                     amountThrees++;
 
                     for(int t = 0; t < 200; t++)
