983f272425ceee24f2f65932a037176f3d5217ff
ci3l4kM4sl4k <dubieltomasz@protonmail.com>
2022-09-21 17:37:37 +0200

    Update 14_06_2022__1_2.cpp

--- 14_06_2022__1_2.cpp
+++ 14_06_2022__1_2.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 using namespace std;
 
+int SumaKwCyfr(int n)
+{
+    int sumaKwCyfr = 0;
+
+    while(n != 0)
+    {
+        sumaKwCyfr += (n%10)*(n%10);
+        n /= 10;
+    }
+
+    return sumaKwCyfr;
+}
+
 int main()
 {
+    int n;
 
+    cout << "Podaj liczbe: ";
+    cin >> n;
+
+    cout << "Suma kwadratow cyfr podanej liczby wynosi: " << SumaKwCyfr(n) << endl;
 
     return 0;
 }
