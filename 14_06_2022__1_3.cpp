3bb9cef4d0ad8157fd9f5f2264f74338bb6d23e5
Tomasz Dubiel <dubieltomasz@protonmail.com>
2022-09-22 20:16:17 +0200

    Update 14_06_2022__1_3.cpp

--- 14_06_2022__1_3.cpp
+++ 14_06_2022__1_3.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
 using namespace std;
 
+bool sumaKwLiczby(int n)
+{
+    int m = n;
+
+    while(m != 1 && m != n)
+    {
+        int r = 0;
+        while(m != 0)
+        {
+            r += (m%10)*(m%10);
+            m /= 10;
+        }
+        m = r;
+    }
+
+    if(m == 1)
+        return false;
+    else
+        return true;
+}
+
 int main()
 {
+    int n;
 
+    cout << "Podaj liczbe: ";
+    cin >> n;
+
+    if(sumaKwLiczby(n) == true)
+        cout << "Podana liczba jest liczba ciekawa" << endl;
+    else
+        cout << "Podana liczba jest liczba nudna" << endl;
 
     return 0;
 }
