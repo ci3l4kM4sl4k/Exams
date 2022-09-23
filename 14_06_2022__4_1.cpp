315d282f67498d93afb46424e9774ced9eeb2fb9
Tomasz Dubiel <dubieltomasz@protonmail.com>
2022-09-23 20:47:21 +0200

    Update 14_06_2022__4_1.cpp

--- 14_06_2022__4_1.cpp
+++ 14_06_2022__4_1.cpp
@@ -1,7 +1,9 @@
+//Algorithm that reflects given numbers (e.g. 1234 to 4321) and than reads them, if they are divisible by 17
 #include <iostream>
 #include <fstream>
 using namespace std;
 
+/////////////////////////////////////function
 void reflection(int arr[100], int n)
 {
     
@@ -20,15 +22,17 @@ void reflection(int arr[100], int n)
             cout << N << endl;
     }
 }
+//////////////////////////////////////
 
 int main()
 {
-    string fileName = "liczby.txt";
+    string fileName = "numbers.txt"; //file name
     fstream file;
     int arr[100];
-    int i = 0;
 
-    file.open(fileName, ios::in);
+    file.open(fileName, ios::in); //imports numbers from file
+        int i = 0;
+
         while(!file.eof())
         {
             file >> arr[i];
@@ -36,7 +40,7 @@ int main()
         }
     file.close();
 
-    reflection(arr, i);
+    reflection(arr, i); //usage of function
 
     return 0;
 }
