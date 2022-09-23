75e803a86198192c301bc14712eab036763d606f
Tomasz Dubiel <dubieltomasz@protonmail.com>
2022-09-23 20:41:21 +0200

    Update 14_06_2022__4_1.cpp

--- 14_06_2022__4_1.cpp
+++ 14_06_2022__4_1.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
+
+void reflection(int arr[100], int n)
+{
+    
+    for(int i = 0; i < n; i++)
+    {
+        int N = 0;
+
+        while(arr[i] != 0)
+        {
+            N *= 10;
+            N += arr[i]%10;
+            arr[i] /= 10;
+        }
+
+        if(N % 17 == 0)
+            cout << N << endl;
+    }
+}
 
 int main()
 {
-    
+    string fileName = "liczby.txt";
+    fstream file;
+    int arr[100];
+    int i = 0;
+
+    file.open(fileName, ios::in);
+        while(!file.eof())
+        {
+            file >> arr[i];
+            i++;
+        }
+    file.close();
+
+    reflection(arr, i);
 
     return 0;
 }
