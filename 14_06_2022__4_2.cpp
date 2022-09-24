6c756a264b45596217b5a698a11a1cd7017b3ec9
Tomasz Dubiel <dubieltomasz@protonmail.com>
2022-09-24 11:18:08 +0200

    Update 14_06_2022__4_2.cpp

--- 14_06_2022__4_2.cpp
+++ 14_06_2022__4_2.cpp
@@ -4,7 +4,30 @@ using namespace std;
 
 int main()
 {
+    string fileName = "przyklad.txt";
+    fstream file;
+    int n, reflection, result, highestResult = 0;
 
+    file.open(fileName, ios::in);
+        while(!file.eof())
+        {
+            file >> n;
+
+            int N = n;
+            while(N != 0)
+            {
+                reflection *= 10;
+                reflection = N % 10;
+                N /= 10;
+            }
+            
+            if(reflection - N > 0)
+                result = reflection - n;
+            else
+                result = n - reflection;
+
+        }
+    file.close();
 
 
     return 0;
