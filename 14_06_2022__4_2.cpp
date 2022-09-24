284ea738e842e9eb12c06398fb26753624a954fe
Tomasz Dubiel <dubieltomasz@protonmail.com>
2022-09-24 11:27:46 +0200

    Update 14_06_2022__4_2.cpp

--- 14_06_2022__4_2.cpp
+++ 14_06_2022__4_2.cpp
@@ -1,34 +1,42 @@
+//
 #include <iostream>
 #include <fstream>
 using namespace std;
 
 int main()
 {
-    string fileName = "przyklad.txt";
+    string fileName = "liczby.txt";
     fstream file;
-    int n, reflection, result, highestResult = 0;
+    int n, reflection, result, number = 0, highestResult = 0;
 
     file.open(fileName, ios::in);
         while(!file.eof())
         {
             file >> n;
 
+            reflection = 0;
             int N = n;
             while(N != 0)
             {
                 reflection *= 10;
-                reflection = N % 10;
+                reflection += N % 10;
                 N /= 10;
             }
             
-            if(reflection - N > 0)
+            if(reflection - n > 0)
                 result = reflection - n;
             else
                 result = n - reflection;
 
+            if(result > highestResult)
+            {
+                number = n;
+                highestResult = result;
+            }
         }
     file.close();
 
+    cout << number << " " << highestResult;
 
     return 0;
 }
