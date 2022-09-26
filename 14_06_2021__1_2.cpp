67f276e05de659468c4da0c48c5f5acaef5b039f
Tomasz Dubiel <dubieltomasz@protonmail.com>
2022-09-26 16:37:07 +0200

    Update 14_06_2021__1_2.cpp

--- 14_06_2021__1_2.cpp
+++ 14_06_2021__1_2.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 using namespace std;
 
+///////////////////////function
+int sumOfSquares(int n)
+{
+    int dl = 0, x = n;
+
+    while(n != 0)
+    {
+        while((x*x) <= n)
+        {
+            n -= (x*x);
+            dl++;
+        }
+        x--;
+    }
+
+    return dl;
+}
+///////////////////////////
+
 int main()
 {
+    int n;
 
+    cout << "Podaj liczbe: ";
+    cin >> n;
+
+    cout << "Dlugosc reprezentacji kwadratowej: " << sumOfSquares(n);
 
     return 0;
 }
