9ee78124f9677ad8ffc6fd6bffd076a1e9fd22a2
Tomasz Dubiel <dubieltomasz@protonmail.com>
2022-09-27 15:14:29 +0200

    Update 14_06_2021__4_1.cpp

--- 14_06_2021__4_1.cpp
+++ 14_06_2021__4_1.cpp
@@ -1,9 +1,25 @@
+//Returns the amount of digits in file 'writings.txt'
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main()
 {
+    string fileName = "writings.txt";
+    fstream file;
+    int amount = 0;
 
+    file.open(fileName, ios::in);
+        while(!file.eof())
+        {
+            char n = file.get();
+
+            if(int(n) >= 48 && int(n) <= 57)
+                amount++;
+        }
+    file.close();
+
+    cout << "Amount of digits in file is: " << amount;
 
     return 0;
 }
