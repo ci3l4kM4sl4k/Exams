8688283d590b13436648af756e5652b882200055
Tomasz Dubiel <dubieltomasz@protonmail.com>
2022-09-28 17:58:48 +0200

    Update 14_06_2021__4_2.cpp

--- 14_06_2021__4_2.cpp
+++ 14_06_2021__4_2.cpp
@@ -1,3 +1,6 @@
+//Reads a fifty-character password from the file 'strings.txt' as follows:
+//- in every twentieth line (in lines numbered 20, 40, 60, ..., 1000), there is exactly one letter of the password;
+//- the hidden letter in the following lines is always in a different position: in the 20th line its the first character, in the 40th row the second symbol, in the 60th row on the third possition, ...
 #include <iostream>
 #include <fstream>
 using namespace std;
