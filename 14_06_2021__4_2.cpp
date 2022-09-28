b9ca1d5aba8069efca5d792d1d495ff4c4ed7bb6
Tomasz Dubiel <dubieltomasz@protonmail.com>
2022-09-28 17:53:07 +0200

    Update 14_06_2021__4_2.cpp

--- 14_06_2021__4_2.cpp
+++ 14_06_2021__4_2.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main()
 {
+    string fileName = "napisy.txt";
+    fstream file;
+    int i = 0, n = 0;
+    string password;
 
+    file.open(fileName, ios::in);
+        while(!file.eof())
+        {
+            file >> password;
+            i++;
 
+            if(i % 20 == 0)
+            {
+                cout << password[n];
+                n++;
+            }
+        }
+    file.close();
 
     return 0;
 }
