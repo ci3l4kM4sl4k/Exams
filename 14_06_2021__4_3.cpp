859513cea2178a907470bed0ffbd06a092de1ab0
Tomasz Dubiel <dubieltomasz@protonmail.com>
2022-09-29 17:22:27 +0200

    Update 14_06_2021__4_3.cpp

--- 14_06_2021__4_3.cpp
+++ 14_06_2021__4_3.cpp
@@ -1,11 +1,11 @@
-//Lists middle letters of palindromes as well as words that would also form palindromes by adding one letter at their beginning or their end
+//Lists middle letters of palindromes as well as words that would also form palindromes by cutting one letter from their beginning or their end
 #include <iostream>
 #include <fstream>
 using namespace std;
 
 int main()
 {
-    string fileName = "napisy.txt";
+    string fileName = "words.txt"; //name of file
     fstream file;
     string word;
     int i = 0, j = 0;
@@ -15,21 +15,21 @@ int main()
         {
             file >> word;
 
-            for (i = 0, j = word.length()-1; i < j; i++, j--)
+            for (i = 0, j = word.length()-1; i < j; i++, j--) //checks if given word is a normal palindrome
                 if (word[i] != word[j])
                     break;
 
             if (i >= j)
                 cout << word[(word.length()/2)-1] << endl;
             ///////////////////////
-            for (i = 1, j = word.length()-1; i < j; i++, j--)
+            for (i = 1, j = word.length()-1; i < j; i++, j--) //checks if given word without its first letter is a palindrome
                 if (word[i] != word[j])
                     break;
 
             if (i >= j)
                 cout << word[(word.length()/2)] << endl;
             ///////////////////////
-            for (i = 0, j = word.length()-2; i < j; i++, j--)
+            for (i = 0, j = word.length()-2; i < j; i++, j--) //checks if given word without its last letter is a palindrome
                 if (word[i] != word[j])
                     break;
 
