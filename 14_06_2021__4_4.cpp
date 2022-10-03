a5b1c94ede2dae457bd12023ea83731512b313e6
Tomasz Dubiel <dubieltomasz@protonmail.com>
2022-10-03 17:47:22 +0200

    Update 14_06_2021__4_4.cpp

--- 14_06_2021__4_4.cpp
+++ 14_06_2021__4_4.cpp
@@ -4,28 +4,38 @@ using namespace std;
 
 int main()
 {
-    string fileName = "przyklad.txt";
+    string fileName = "napisy.txt";
     fstream file;
     char x, y;
+    int n;
 
     file.open(fileName, ios::in);
         while(!file.eof())
         {
-            x = file.get();
+            n = 0;
 
-            if(48 <= int(x) && int(x) <= 57)
-            {
+                do
+                {
+                    x = file.get();
+                    n++;
+                }
+                while(48 > int(x) && n < 50 || int(x) > 57 && n < 50);
+                
                 do
                 {
                     y = file.get();
+                    n++;
                 }
-                while(48 > int(y) || int(y) > 57);
-                
-                int z = (int(x)-48)*10+(int(y)-48);
+                while(48 > int(y) && n < 50 || int(y) > 57 && n < 50);
 
-                if(65 <= z && z <= 90)
-                    cout << char(z);
-            }
+                if(n < 50)
+                {    
+                    int z = (int(x)-48)*10+(int(y)-48);
+
+                    if(65 <= z && z <= 90)
+                        cout << char(z);
+                }
+                
         }
     file.close();
 
