47397c395b5e7d57cc4e2e87ddfedcd80c66a3db
Tomasz Dubiel <dubieltomasz@protonmail.com>
2022-10-04 19:05:43 +0200

    Update 19_05_2021__1_2.cpp

--- 19_05_2021__1_2.cpp
+++ 19_05_2021__1_2.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 using namespace std;
 
+int dopelnienie(int n)
+{
+    int d = 0, m = 1;
+
+    while(n > 0)
+    {
+        d += (9-n%10)*m;
+        n /= 10;
+        m *= 10;
+    }
+
+    return d;
+}
+
 int main()
 {
+    int n;
 
+    cout << "Podaj liczbe: ";
+    cin >> n;
+
+    cout << "Dopelnieniem podanej liczby jest liczba: " << dopelnienie(n);
 
     return 0;
 }
