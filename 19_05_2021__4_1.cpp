ac85802ed87028e1caf094dd6815a38b65fe7b48
Tomasz Dubiel <dubieltomasz@protonmail.com>
2022-10-05 20:30:25 +0200

    Update 19_05_2021__4_1.cpp

--- 19_05_2021__4_1.cpp
+++ 19_05_2021__4_1.cpp
@@ -1,28 +1,29 @@
+//an algorithm that calculates the total length of the string after executing all instructions from the file 'instructions.txt'
 #include <iostream>
 #include <fstream>
 using namespace std;
 
 int main()
 {
-    string fileName = "instrukcje.txt";
+    string fileName = "instructions.txt"; //name of file
     fstream file;
     string word;
-    int dlugosc = 0;
+    int length = 0;
 
     file.open(fileName, ios::in);
         while(!file.eof())
         {
             file >> word;
 
-            if(word == "DOPISZ")
-                dlugosc++;
+            if(word == "DOPISZ") //if instruction says to add a letter to string
+                length++;
 
-            if(word == "USUN")
-                dlugosc--;
+            if(word == "USUN") //if instruction says to delete a letter from string
+                length--;
         }
     file.close();
 
-    cout << "Dlugosc powstalego napisu wynosi: " << dlugosc;
+    cout << "Length of the string: " << length;
 
     return 0;
 }
