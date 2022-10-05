fda903e3801d80c6c29c10d7296cc20837d48bb6
Tomasz Dubiel <dubieltomasz@protonmail.com>
2022-10-05 20:26:50 +0200

    Update 19_05_2021__4_1.cpp

--- 19_05_2021__4_1.cpp
+++ 19_05_2021__4_1.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main()
 {
+    string fileName = "instrukcje.txt";
+    fstream file;
+    string word;
+    int dlugosc = 0;
 
+    file.open(fileName, ios::in);
+        while(!file.eof())
+        {
+            file >> word;
+
+            if(word == "DOPISZ")
+                dlugosc++;
+
+            if(word == "USUN")
+                dlugosc--;
+        }
+    file.close();
+
+    cout << "Dlugosc powstalego napisu wynosi: " << dlugosc;
 
     return 0;
 }
