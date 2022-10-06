5d4626764a5d9918eb721d4e42a7739559f87c2c
Tomasz Dubiel <dubieltomasz@protonmail.com>
2022-10-06 13:49:48 +0200

    Update 19_05_2021__4_2.cpp

--- 19_05_2021__4_2.cpp
+++ 19_05_2021__4_2.cpp
@@ -4,7 +4,40 @@ using namespace std;
 
 int main()
 {
+    string fileName = "instrukcje.txt";
+    fstream file;
+    string word, last = "x", inMax;
+    int max = 0, now, i = 0;
 
+    file.open(fileName, ios::in);
+        while(!file.eof())
+        {
+            file >> word;
+
+            if(i % 2 == 0)
+            {
+                if(word == last)
+                {
+                    now++;
+
+                    if(max < now)
+                    {
+                        inMax = word;
+                        max = now;
+                    }
+                }
+                else
+                {
+                    last = word;
+                    now = 1;
+                }
+            }
+
+            i++;
+        }
+    file.close();
+
+    cout << "The kind of the instruction - " << inMax << ", its amount - " << max << ".";
 
     return 0;
 }
