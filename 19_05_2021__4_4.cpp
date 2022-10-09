c62a5301da1371ea82455529b2443c6970f8c12e
Tomasz Dubiel <dubieltomasz@protonmail.com>
2022-10-09 20:48:39 +0200

    Update 19_05_2021__4_4.cpp

--- 19_05_2021__4_4.cpp
+++ 19_05_2021__4_4.cpp
@@ -4,8 +4,53 @@ using namespace std;
 
 int main()
 {
+    string fileName = "instrukcje.txt";
+    fstream file;
+    string instrukcja;
+    char letter, napis[1000];
+    int i = 0, n = 0;
 
+    file.open(fileName, ios::in);
+        while(!file.eof())
+        {
+            if(i % 2 == 0)
+            {
+                file >> instrukcja;
+            }
+            else
+            {
+                file >> letter;
 
+                if(instrukcja == "DOPISZ")
+                {
+                    napis[n] = letter;
+                    n++;
+                }
+
+                if(instrukcja == "ZMIEN")
+                    napis[n-1] = letter;
+
+                if(instrukcja == "USUN")
+                    n--;
+
+                if(instrukcja == "PRZESUN")
+                    for(int j = 0; j < n; j++)
+                        if(napis[j] == letter)
+                        {
+                            napis[j] = char(int(napis[j])+1);
+
+                            if(int(napis[j]) > 90)
+                                napis[j] = char(int(napis[j])-26);
+
+                            break;
+                        }
+            }
+
+            i++;
+        }
+    file.close();
+
+    cout << napis;
 
     return 0;
 }
