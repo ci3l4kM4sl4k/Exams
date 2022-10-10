b77c1308761f533cf1e3df3ca6037c2094910d57
Tomasz Dubiel <dubieltomasz@protonmail.com>
2022-10-10 21:26:12 +0200

    Update 00_03_2021__1_2.cpp

--- 00_03_2021__1_2.cpp
+++ 00_03_2021__1_2.cpp
@@ -1,9 +1,54 @@
 #include <iostream>
 using namespace std;
 
+int decToBin(int x)
+{
+    int y = 0, i = 1;
+
+    while(x != 0)
+    {
+        y += (x%2) * i;
+        x /= 2;
+        i *= 10;
+    }
+
+  return y;
+}
+
+int turniej(int k, int x, int y)
+{
+    int z = 0;
+
+    if(x > y)
+        while(x != 0)
+        {
+            x /= 10;
+            z++;
+        }
+    else
+        while(y != 0)
+        {
+            y /= 10;
+            z++;
+        }
+
+    return z;
+}
+
 int main()
 {
+    int k, x, y;
 
-    
+    cout << "Podaj ilosc rund: ";
+    cin >> k;
+
+    cout << "Podaj numer druzyny1: ";
+    cin >> x;
+
+    cout << "Podaj numer druzyny2: ";
+    cin >> y;
+
+    cout << turniej(k, decToBin(x), decToBin(y));
+
     return 0;
 }
