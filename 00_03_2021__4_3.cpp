26d9a96c09a079784ea5b5748107cd5bb4cd0394
Tomasz Dubiel <dubieltomasz@protonmail.com>
2022-10-15 16:44:25 +0200

    Update 00_03_2021__4_3.cpp

--- 00_03_2021__4_3.cpp
+++ 00_03_2021__4_3.cpp
@@ -1,7 +1,11 @@
+// Algorithm that calculates in which city will the gallery with the largest number of different types of premises be built
+//and in which the gallery with the smallest number of different types of premises?
+//We will say that two premises are of the same type if their surface area is the same.
 #include <iostream>
 #include <fstream>
 using namespace std;
 
+///////////////////////////////////sorts array
 void sort(int array[], int size)
 {
     for(int i = 0; i < size; i++)
@@ -9,7 +13,9 @@ void sort(int array[], int size)
             if(array[j] > array[j+1])
                 swap(array[j],array[j+1]);
 }
+/////////////////////////////////////
 
+///////////////////////////////counts amount of different premises
 int count(int array[], int size)
 {
     int x = 0, last = 0;
@@ -23,13 +29,14 @@ int count(int array[], int size)
 
     return x;
 }
+////////////////////////////////
 
 int main()
 {
-    string fileName = "galerie.txt";
+    string fileName = "galleries.txt"; //name of file
     fstream file;
-    string cityWithHighiest, cityWithLowest;
-    int highiestAmount = 0, lowestAmount = 10000, size = 70, surfaces[size];
+    string cityWithHighiest, cityWithLowest; //variables for cities where will the gallery with the largest and smallest number of different types of premises be built
+    int highiestAmount = 0, lowestAmount = 10000, size = 70, surfaces[size]; //variables for highist and smallest amount of premises, size of array, array for sizes of premises
 
     file.open(fileName, ios::in);
         while(!file.eof())
