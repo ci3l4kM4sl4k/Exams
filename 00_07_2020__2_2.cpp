31fdcb8657ed46160da6bbfc75404557edadd624
Tomasz Dubiel <dubieltomasz@protonmail.com>
2022-10-16 17:43:52 +0200

    Update 00_07_2020__2_2.cpp

--- 00_07_2020__2_2.cpp
+++ 00_07_2020__2_2.cpp
@@ -1,9 +1,30 @@
+//Algorithm for Egyptian fractions
 #include <iostream>
 using namespace std;
 
+///////////////////////////function
+void egyptianFractions(int p, int q)
+{
+    while(p>0)
+    {
+        int n = 0;
+        while(n < (float) q/p)
+            n++;
+
+        cout << n << " ";
+        p = (p*n-q);
+        q *= n;
+    }
+}
+///////////////////////////
+
 int main()
 {
+    int p, q;
+    cout << "Enter numbers: " << endl;
+    cin >> p >> q;
 
+    egyptianFractions(p, q);
 
     return 0;
 }
